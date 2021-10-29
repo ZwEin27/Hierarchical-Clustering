2ff62d5e54f122e23f06d73bff0f60ba339dec3b
Swarup Kharul <swarupkharulsk@gmail.com>
2021-10-29 14:49:16 +0530

    fix: error is sort function

--- hclust.py
+++ hclust.py
@@ -18,6 +18,8 @@ import math
 import os
 import heapq
 import itertools
+from collections import OrderedDict
+
 
 class Hierarchical_Clustering:
     def __init__(self, ipt_data, ipt_k):
@@ -39,7 +41,9 @@ class Hierarchical_Clustering:
         if not os.path.isfile(self.input_file_name):
             self.quit("Input file doesn't exist or it's not a file")
 
-        self.dataset, self.clusters, self.gold_standard = self.load_data(self.input_file_name)
+        self.dataset, self.clusters, self.gold_standard = self.load_data(
+            self.input_file_name
+        )
         self.dataset_size = len(self.dataset)
 
         if self.dataset_size == 0:
@@ -56,10 +60,10 @@ class Hierarchical_Clustering:
         if self.dimension == 0:
             self.quit("dimension for dataset cannot be zero")
 
-    """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """"""
     """                      Hierarchical Clustering Functions                       """
-    """                                                                              """    
-    """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    """                                                                              """
+    """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """"""
 
     def euclidean_distance(self, data_point_one, data_point_two):
         """
@@ -70,8 +74,8 @@ class Hierarchical_Clustering:
         size = len(data_point_one)
         result = 0.0
         for i in range(size):
-            f1 = float(data_point_one[i])   # feature for data one
-            f2 = float(data_point_two[i])   # feature for data two
+            f1 = float(data_point_one[i])  # feature for data one
+            f2 = float(data_point_two[i])  # feature for data two
             tmp = f1 - f2
             result += pow(tmp, 2)
         result = math.sqrt(result)
@@ -80,16 +84,16 @@ class Hierarchical_Clustering:
     def compute_pairwise_distance(self, dataset):
         result = []
         dataset_size = len(dataset)
-        for i in range(dataset_size-1):    # ignore last i
-            for j in range(i+1, dataset_size):     # ignore duplication
+        for i in range(dataset_size - 1):  # ignore last i
+            for j in range(i + 1, dataset_size):  # ignore duplication
                 dist = self.euclidean_distance(dataset[i]["data"], dataset[j]["data"])
 
                 # duplicate dist, need to be remove, and there is no difference to use tuple only
                 # leave second dist here is to take up a position for tie selection
-                result.append( (dist, [dist, [[i], [j]]]) )
+                result.append((dist, [dist, [[i], [j]]]))
 
         return result
-                
+
     def build_priority_queue(self, distance_list):
         heapq.heapify(distance_list)
         self.heap = distance_list
@@ -98,7 +102,7 @@ class Hierarchical_Clustering:
     def compute_centroid_two_clusters(self, current_clusters, data_points_index):
         size = len(data_points_index)
         dim = self.dimension
-        centroid = [0.0]*dim
+        centroid = [0.0] * dim
         for index in data_points_index:
             dim_data = current_clusters[str(index)]["centroid"]
             for i in range(dim):
@@ -110,7 +114,7 @@ class Hierarchical_Clustering:
     def compute_centroid(self, dataset, data_points_index):
         size = len(data_points_index)
         dim = self.dimension
-        centroid = [0.0]*dim
+        centroid = [0.0] * dim
         for idx in data_points_index:
             dim_data = dataset[idx]["data"]
             for i in range(dim):
@@ -150,9 +154,9 @@ class Hierarchical_Clustering:
                 del current_clusters[str(pair_item)]
             self.add_heap_entry(heap, new_cluster, current_clusters)
             current_clusters[str(new_cluster_elements)] = new_cluster
-        current_clusters.sort()
-        return current_clusters
-            
+        # current_clusters.sort()
+        return OrderedDict(sorted(current_clusters.items()))
+
     def valid_heap_node(self, heap_node, old_clusters):
         pair_dist = heap_node[0]
         pair_data = heap_node[1]
@@ -160,11 +164,13 @@ class Hierarchical_Clustering:
             if old_cluster in pair_data:
                 return False
         return True
-            
+
     def add_heap_entry(self, heap, new_cluster, current_clusters):
         for ex_cluster in current_clusters.values():
             new_heap_entry = []
-            dist = self.euclidean_distance(ex_cluster["centroid"], new_cluster["centroid"])
+            dist = self.euclidean_distance(
+                ex_cluster["centroid"], new_cluster["centroid"]
+            )
             new_heap_entry.append(dist)
             new_heap_entry.append([new_cluster["elements"], ex_cluster["elements"]])
             heapq.heappush(heap, (dist, new_heap_entry))
@@ -192,36 +198,37 @@ class Hierarchical_Clustering:
         if tp_fp == 0:
             precision = 0.0
         else:
-            precision = tp/tp_fp
+            precision = tp / tp_fp
         if tp_fn == 0:
             precision = 0.0
         else:
-            recall = tp/tp_fn
+            recall = tp / tp_fn
 
         return precision, recall
 
-    """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """"""
     """                             Helper Functions                                 """
-    """                                                                              """    
-    """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    """                                                                              """
+    """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """"""
+
     def load_data(self, input_file_name):
         """
         load data and do some preparations
 
         """
-        input_file = open(input_file_name, 'rU')
+        input_file = open(input_file_name, "r")
         dataset = []
         clusters = {}
         gold_standard = {}
         id = 0
         for line in input_file:
-            line = line.strip('\n')
+            line = line.strip("\n")
             row = str(line)
             row = row.split(",")
             iris_class = row[-1]
 
             data = {}
-            data.setdefault("id", id)   # duplicate
+            data.setdefault("id", id)  # duplicate
             data.setdefault("data", row[:-1])
             data.setdefault("class", row[-1])
             dataset.append(data)
@@ -238,7 +245,9 @@ class Hierarchical_Clustering:
         return dataset, clusters, gold_standard
 
     def quit(self, err_desc):
-        raise SystemExit('\n'+ "PROGRAM EXIT: " + err_desc + ', please check your input' + '\n')
+        raise SystemExit(
+            "\n" + "PROGRAM EXIT: " + err_desc + ", please check your input" + "\n"
+        )
 
     def loaded_dataset(self):
         """
@@ -248,20 +257,19 @@ class Hierarchical_Clustering:
         return self.dataset
 
     def display(self, current_clusters, precision, recall):
-        print precision
-        print recall
+        print(precision)
+        print(recall)
         clusters = current_clusters.values()
         for cluster in clusters:
             cluster["elements"].sort()
-            print cluster["elements"]
+            print(cluster["elements"])
 
 
-
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"""""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """"""
 """                               Main Method                                    """
-"""                                                                              """    
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-if __name__ == '__main__':
+"""                                                                              """
+"""""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """"""
+if __name__ == "__main__":
     """
     inputs:
     - ipt_data: a text file name for the input data
@@ -279,8 +287,8 @@ if __name__ == '__main__':
     # ipt_data = "iris_dataset1.txt"
     # ipt_k = 3
 
-    ipt_data = sys.argv[1]      # input data, e.g. iris.dat
-    ipt_k = int(sys.argv[2])    # number of clusters, e.g. 3
+    ipt_data = sys.argv[1]  # input data, e.g. iris.dat
+    ipt_k = int(sys.argv[2])  # number of clusters, e.g. 3
 
     hc = Hierarchical_Clustering(ipt_data, ipt_k)
     hc.initialize()
@@ -301,14 +309,10 @@ if __name__ == '__main__':
     # distance_list = hc.compute_pairwise_distance()
     # distance_list.sort()
     # print distance_list
-    
+
     ## heapq test
     # heap = []
     # data = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0]
     # data = [[1,4,5], [3,6,1], [5,6,10], [7,2,11], [9,6,1], [2,1,5], [4,2,1], [6,6,5], [8,7,1], [0,1,0]]
     # heapq.heapify(data)
     # print data
-
-
-
-
